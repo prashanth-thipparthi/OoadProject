a05cadf64b72371a3523b1298c7d7ff018f16067
amith1893 <amith1893@gmail.com>
2019-04-21 15:11:38 -0600

    Removed id as the primary key, made username as the primary key

--- ooad/src/main/java/com/example/demo/model/Login.java
+++ ooad/src/main/java/com/example/demo/model/Login.java
@@ -2,28 +2,31 @@ package com.example.demo.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-@Entity
+@Entity(name = "login")
 @Table(name = "login")
 public class Login {
 	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private int id;
+	
 	@Id
-	@GeneratedValue
-	private int id;
 	private String username;
 	private String password;
 	private boolean flag;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+//	public int getId() {
+//		return id;
+//	}
+//	public void setId(int id) {
+//		this.id = id;
+//	}
 	public String getUsername() {
 		return username;
 	}
@@ -42,6 +45,5 @@ public class Login {
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
-	
-	
+
 }
