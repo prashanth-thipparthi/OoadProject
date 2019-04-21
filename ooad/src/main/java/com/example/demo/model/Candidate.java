7933481a56aa2d49c9de9782915fe4b6ae6e5f01
amith1893 <amith1893@gmail.com>
2019-04-21 15:09:25 -0600

    Added skills and username as columns, made username as foreign key for login table

--- ooad/src/main/java/com/example/demo/model/Candidate.java
+++ ooad/src/main/java/com/example/demo/model/Candidate.java
@@ -4,26 +4,59 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-@Entity
+@Entity(name = "candidates")
 @Table(name = "candidates")
 public class Candidate {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int candidate_id;
 	
 	private String description;
-
+	private String candidatename;
+	private String skills;
+	
+	
+	@OneToOne
+	@JoinColumn(name="username")
+	private Login user;
+	
 	public String getDescription() {
 		return description;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public Login getUser() {
+		return user;
+	}
+
+	public void setUser(Login user) {
+		this.user = user;
+	}
+
+	public String getCandidatename() {
+		return candidatename;
+	}
+
+	public void setCandidatename(String candidatename) {
+		this.candidatename = candidatename;
+	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
 	}
 
 }
