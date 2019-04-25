0d6ca7c0b76867a3f39391191599b624dd24a70a
amith1893 <amith1893@gmail.com>
2019-04-24 21:34:17 -0600

    Added files  for parsing request parameters for POST requests

---
+++ ooad/src/main/java/com/example/demo/AddApplicationParameters.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+public class AddApplicationParameters {
+
+	private String job_id;
+	private String candidate_id;
+	private String status;
+		
+	public String getJob_id() {
+		return job_id;
+	}
+	public void setJob_id(String job_id) {
+		this.job_id = job_id;
+	}
+	
+	public String getCandidate_id() {
+		return candidate_id;
+	}
+	public void setCandidate_id(String candidate_id) {
+		this.candidate_id = candidate_id;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+}
---
+++ ooad/src/main/java/com/example/demo/AddJobParameters.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class AddJobParameters {
+
+	private String skills;
+	private String location;
+	private String description;
+	private String role;
+	private int company_id;
+	public String getSkills() {
+		return skills;
+	}
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public int getCompany_id() {
+		return company_id;
+	}
+	public void setCompany_id(int company_id) {
+		this.company_id = company_id;
+	}
+	
+}
