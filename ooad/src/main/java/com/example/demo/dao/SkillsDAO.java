f59f0f89e9ab28ead2d1c55e39e9a8942d1241b4
amith1893 <amith1893@gmail.com>
2019-04-26 00:12:14 -0600

    Added comments in SkillsDAO.java

--- ooad/src/main/java/com/example/demo/dao/SkillsDAO.java
+++ ooad/src/main/java/com/example/demo/dao/SkillsDAO.java
@@ -3,6 +3,10 @@
  */
 package com.example.demo.dao;
 
+/*
+ * This class contains functions which performs CRUD operations on the Skills table in the database
+ */
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Skills;
@@ -10,7 +14,10 @@ import java.lang.String;
 import java.util.List;
 
 public interface SkillsDAO extends JpaRepository<Skills,String>{
-	//List<Skills> findA
+
+	/* 
+	 * Finds all the skills in the skills table
+	 */
 	List<Skills> findAll();
 
 }
